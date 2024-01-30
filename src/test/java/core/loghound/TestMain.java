b6d4527ce3bdf0c320eb51e2586f3f4183b82885
zheyuanlin <lzy9986@126.com>
2024-01-30 20:02:40 +0800

    init

---
+++ src/test/java/core/loghound/TestMain.java
@@ -0,0 +1,11 @@
+package core.loghound;
+
+import org.junit.*;
+
+public class TestMain {
+    @Test
+    public void testMain(){
+        Assert.assertEquals(2,1+1);
+        Assert.fail("This test is not ready");
+    }
+}
